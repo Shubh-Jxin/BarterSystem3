153306619b0010bd156f3da221679b79f460a5ad
Shubh Jain <65031443+Shubh-Jxin@users.noreply.github.com>
2021-05-05 19:15:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen'
+
+export default function App() {
+  return (
+    <SignupLoginScreen/>
+  );
+}
+
---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBgBZVGd7P27FhSJCdpIeYWEmAczXEJ_1c",
+    authDomain: "bartersystem-aea6f.firebaseapp.com",
+    projectId: "bartersystem-aea6f",
+    storageBucket: "bartersystem-aea6f.appspot.com",
+    messagingSenderId: "993769644035",
+    appId: "1:993769644035:web:88eed12da79538ec166e36"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
